0729b465f7321cb4efe79f266b337386761f5e36
John Quarmor <johnequarmor@gmail.com>
2020-01-24 06:27:33 +0000

    Started working on Javascript file, made some minor changes to index page and CSS.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,17 @@
-//Debugger function:
-function clickTest(){
-	alert('Clicked!');
+const choices = document.querySelectorAll(".choice");
+const score = document.getElementById("score");
+const result = document.getElementById("result");
+const restart = document.getElementById("restart");
+const modal = document.querySelector(".modal");
+const scoreboard = {
+	player: 0,
+	computer: 0
 }
 
+// Play game
+function play(e){
+	console.log(e.target.id);
+}
+
+// Event Listeners
+choices.forEach(choice => choice.addEventListener('click', play));
